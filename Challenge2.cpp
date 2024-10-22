b39afd29344adcc64d64dddeb19063caff9aaa20
Qiao Zhaochen <51873940+JoeChen322@users.noreply.github.com>
2024-10-22 23:51:44 +0200

    Update Challenge2.cpp

--- Challenge2.cpp
+++ Challenge2.cpp
@@ -65,25 +65,42 @@ int main(int argc, char *argv[])
 
 /*------------------------Q2.Canculate ATAX=LAMDAX-----------------------------*/
    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(ATA);
-   //VectorXd eigenValues =solver.eigenvalues();
-  // std::cout<<"Top2 MAX Singular Values of A:" <<sqrt(eigenValues(eigenValues.size() - 1))<<" , "
-   //<<sqrt(eigenValues(eigenValues.size() - 2))<<std::endl;
+   VectorXd eigenValues =solver.eigenvalues();
+   std::cout<<"Top2 MAX Singular Values of A:" <<sqrt(eigenValues(eigenValues.size() - 1))<<" , "
+   <<sqrt(eigenValues(eigenValues.size() - 2))<<std::endl;
 /*------------------------Q3.Agreement-----------------------------------------*/
     std::cout<<"result is in agreement with the previous one"<<std::endl;
-   
-/*------------------------Q5.diagnol of SVD------------------------------------*/  
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
-    //Eigen::VectorXd singularValues = svd.singularValues();  // 奇异值向量
-    //std::cout<<"norm of diagnal:"<<singularValues.norm()<<std::endl;
-
+/*------------------------Q4 is run by lis-------------------------------------*/  
+/*------------------------Q5.6,7 diagnol of SVD------------------------------------*/  
+   /* Eigen::BDCSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    MatrixXd U=svd.matrixU();
+    MatrixXd V=svd.matrixV();
+    MatrixXd sigma = svd.singularValues().asDiagonal();
+    MatrixXd C= U.leftCols(80);
+    MatrixXd D = V.leftCols(80) * svd.singularValues().head(80).asDiagonal();
+    std::cout<<"norm of sigma:"<<sigma.norm()<<std::endl;
+    std::cout << "Nonzero Entries For Matrix C80: " << C.nonZeros() << std::endl;
+    std::cout << "Nonzero Entries For Matrix D80: " << D.nonZeros() << std::endl;
+    MatrixXd img=C*(D.transpose());
+    img = img.cwiseMin(1).cwiseMax(0);
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> img_output = img.unaryExpr(
+        [](double pixel)
+        { return static_cast<unsigned char>(pixel * 255.0); });
+    const std::string output_image_path1 = "CDT80.png";
+  if (stbi_write_png(output_image_path1.c_str(), width, height, 1,img_output.data(), width) == 0)
+   {
+    std::cerr << "Error: Could not save grayscale image" << std::endl;
+    return 1;
+  }
+  std::cout << "CDT image saved to " << output_image_path1 << std::endl;*/
 /*------------------------Q8.diagnol of SVD------------------------------------*/ 
 /*------------------------Q9.add noise to checkboard---------------------------*/
     MatrixXd noise;
     noise=MatrixXd::Random(200,200);
     noise=50*noise;
-    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> checkerboard(200, 200);
     
-    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noise_checkerboard(200, 200);
+    Matrix<double, Dynamic, Dynamic, RowMajor> checkerboard(200, 200);    
+    Matrix<double, Dynamic, Dynamic, RowMajor> noise_checkerboard(200, 200);
     // Create checkerboard
     for(int i=0;i<200;++i)
     {
@@ -95,32 +112,43 @@ int main(int argc, char *argv[])
                 checkerboard(i, j) =0; 
                 if((checkerboard(i, j)+noise(i,j))<=0)
                 {noise_checkerboard(i,j)=0;}
-                else noise_checkerboard(i,j)= checkerboard(i, j)+noise(i,j);
+                else noise_checkerboard(i,j)= (checkerboard(i, j)+noise(i,j))/255.0;
             } 
             else 
             {
                 checkerboard(i, j) = 255; 
                 if((checkerboard(i, j)+noise(i,j))>=255)
-                {noise_checkerboard(i,j)=255;}
-                else noise_checkerboard(i,j)= checkerboard(i, j)+noise(i,j);
+                {noise_checkerboard(i,j)=1;}
+                else noise_checkerboard(i,j)=(checkerboard(i, j)+noise(i,j)) /255.0;
             }
         }
     }
- 
-      
-
-  const std::string output_image_path1 = "noise_checkboard.png";
-  if (stbi_write_png(output_image_path1.c_str(), 200, 200, 1,noise_checkerboard.data(), 200) == 0)
+    
+    Eigen::BDCSVD<Eigen::MatrixXd> checkboard_svd(noise_checkerboard, ComputeThinU | ComputeThinV);
+    MatrixXd checkU=checkboard_svd.matrixU();
+    MatrixXd checkV=checkboard_svd.matrixV();
+    MatrixXd checksigma = checkboard_svd.singularValues().asDiagonal();
+    std::cout<<"top 2 singular value of checkboard"<<checkboard_svd.singularValues()[0]<<","<<checkboard_svd.singularValues()[1]<<std::endl;
+    MatrixXd checkC= checkU.leftCols(10);
+    MatrixXd checkD = checkV.leftCols(10) * checkboard_svd.singularValues().head(10).asDiagonal();
+    std::cout<<"norm of sigma:"<<checksigma.norm()<<std::endl;
+    MatrixXd img=checkC*(checkD.transpose());
+    img = img.cwiseMin(1).cwiseMax(0);
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> img_output = img.unaryExpr(
+        [](double pixel)
+        { return static_cast<unsigned char>(pixel * 255.0); });
+    const std::string output_image_path1 = "CDT10.png";
+  if (stbi_write_png(output_image_path1.c_str(), 200, 200, 1,img_output.data(), 200) == 0)
    {
     std::cerr << "Error: Could not save grayscale image" << std::endl;
     return 1;
   }
-
+  std::cout << "CDT image saved to " << output_image_path1 << std::endl;
 
   //turn matrix into vector
 
 
-  std::cout << "Grayscale image saved to " << output_image_path1 << std::endl;
+  
     
     return 0;
 }
